ce9f9d0ea631eaa27226b6dde2edee27e7b0103f
Artur Ghidora <artur.ghidora@arobs.ro>
2024-12-16 22:19:32 +0200

    init

---
+++ Controllers/EmailController.cs
@@ -0,0 +1,38 @@
+﻿using MailKit.Net.Smtp;
+using MimeKit;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Mail;
+using WebApi.Models;
+
+[Route("api/[controller]")]
+[ApiController]
+public class EmailController : ControllerBase
+{
+    [HttpPost("send-email")]
+    public IActionResult SendEmail([FromBody] EmailRequest request)
+    {
+        try
+        {
+            //var email = new MimeMessage();
+            //email.From.Add(MailboxAddress.Parse("example@example.com"));
+            //email.To.Add(MailboxAddress.Parse(request.Email));
+            //email.Subject = "Contact Form Submission";
+            //email.Body = new TextPart("plain")
+            //{
+            //    Text = $"Name: {request.Name}\nMessage: {request.Message}"
+            //};
+
+            //using var smtp = new MailKit.Net.Smtp.SmtpClient();
+            //smtp.Connect("smtp.gmail.com", 587, false);
+            //smtp.Authenticate("your-email@example.com", "your-email-password");
+            //smtp.Send(email);
+            //smtp.Disconnect(true);
+
+            return Ok(new { message = "Email sent successfully!" });
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(new { message = ex.Message });
+        }
+    }
+}
---
+++ Models/EmailRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApi.Models
+{
+    public class EmailRequest
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Message { get; set; }
+    }
+
+}
